6cfc84907f2251032e32a273497b2f719e053ccf
Andres Eduardo Perez <andrese.perezm@uqvirtual.edu.co>
2026-05-24 18:58:47 -0500

    last commit

--- app/src/main/java/com/example/proyectofinaldisenomovil/features/userFlow/home/HomeScreen.kt
+++ app/src/main/java/com/example/proyectofinaldisenomovil/features/userFlow/home/HomeScreen.kt
@@ -369,6 +369,7 @@ fun EventCard(
                         .padding(horizontal = 12.dp, vertical = 4.dp)
                 ) {
                     Text(
+                        //a
                         text = categoryLabel,
                         color = Color.White,
                         fontSize = 13.sp,
